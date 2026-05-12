e5430b6c7da55bfd6e93de6160db58acd56e0dea
Youssef-ahim <youssefahkim1234@gmail.com>
2026-05-12 10:44:06 +0100

    chore: remove scratch scripts causing build errors

--- extract.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('pb_data/data.db', 'utf8');
-
-// Find all schema definitions containing "name":"some_field"
-const matches = [...data.matchAll(/"name":"([^"]+)"/g)].map(m => m[1]);
-console.log("Found fields:", [...new Set(matches)]);
